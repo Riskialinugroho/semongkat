0ca32d940a1ccf7da9297c83e717b3d27426efeb
Riskialinugroho <arisaniriski13@gmail.com>
2023-10-22 02:51:13 +0800

    save 2

---
+++ main.js
@@ -0,0 +1,13 @@
+const mau = document.getElementById('btnMau');
+mau.addEventListener('click', function () {
+    alert('terima kasih')
+})
+
+const button = document.getElementById('btn');
+button.addEventListener('mouseover', function () {
+    button.style.left = `${Math.ceil(Math.random() * 90)}%`;
+    button.style.top = `${Math.ceil(Math.random() * 90)}%`;
+});
+button.addEventListener('click', function () {
+    alert('you clicked me')
+})
